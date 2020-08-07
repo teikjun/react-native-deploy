63d69c4c38f197ca15de851707e0fbb0ccbee0b5
Darsh <darshkpatel@gmail.com>
2020-08-07 16:17:36 +0530

    HotFix: absolute paths in config

--- v2-website/docusaurus.config.js
+++ v2-website/docusaurus.config.js
@@ -341,13 +341,13 @@ module.exports = {
           editUrl:
             'https://github.com/facebook/react-native-website/blob/master/docs/',
           path: '../docs',
-          sidebarPath: './v2-website/sidebars.json',
+          sidebarPath: '../sidebars.json',
         },
         blog: {
           path: 'blog',
         },
         theme: {
-          customCss: './v2-website/src/css/customTheme.css',
+          customCss: '../src/css/customTheme.css',
         },
       },
     ],
